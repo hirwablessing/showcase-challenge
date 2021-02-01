ffcff07e8ff4c3414c624e97c3e22709bc67b7d5
hirwablessing <hirwablessin7@gmail.com>
2021-02-01 12:08:36 +0200

    Fix: warning

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -8,7 +8,7 @@ import { selectUserFromLocalStorage } from './features/user/userSlice'
 const Routes: React.FC = () => {
   const UserFromLocalStorage = useSelector(selectUserFromLocalStorage) || ""
   useEffect(() => {
-    console.log = console.warn = () => { };
+    console.log = console.warn = console.error = () => { };
 
   }, [])
 
